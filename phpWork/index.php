4856e267723e34bc033bb1a240e82ea18c64410d
YashminSainju <yashmin.sainju@selu.edu>
2016-10-24 23:34:57 -0500

    basic phpsketch

---
+++ phpWork/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yashmin
+ * Date: 10/24/2016
+ * Time: 8:20 AM
+ */
+$jobPostings = null;
+include("inc/data.php");
+include("inc/functions.php");
+
+
+include("inc/header.php");
+include ("inc/firstSection.php")
+?>
+
+<?php include("inc/lastSection.php");?>
+<?php include("inc/footer.php"); ?>
