0f761758b629ab273a5f34e1adb53b4df47ebd3d
Sleeper221 <moorma.aar@gmail.com>
2016-10-25 01:45:33 -0500

    quick calendar
    Just proof of concept.

---
+++ hello.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+ </body>
+</html>
