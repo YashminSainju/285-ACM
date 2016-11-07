8c945c7aa10c49d712f9d73deaffb677e1532363
restrepo64 <alexandra.restrepo@selu.edu>
2016-11-07 14:45:31 -0600

    added angularjs to route pages successfully
    can successfully navigate the navbar to different pages. just need to change html files to adapt to one page

---
+++ script.js
@@ -0,0 +1,25 @@
+var routerApp=angular.module('routerApp', ['ui.router']);
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
+	
+	$stateProvider
+	
+		.state('home', {
+			url: '/home',
+			templateUrl:'pages/home.html'
+		})
+		.state('about', {
+			url: '/about',
+			templateUrl:'https://www.acm.org/'
+		})
+		
+		.state('calendar', {
+			url: '/calendar',
+			templateUrl:'pages/calendar.html'
+		})
+	
+		.state('slugo', {
+			url: '/slugo',
+			templateUrl:'pages/slugo.html'
+		});
+});
