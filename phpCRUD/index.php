1300bb6962d21454adf08e5d697ad8999b0a1855
ZOstrowski <w0641076@selu.edu>
2016-11-10 15:34:23 -0600

    CRUD for Members. NOT FINISHED!
    Stuff for CRUD of members. Frontend Materialize. Uses AngularJS and jquery. NOT FINISHED AT ALL!!!!

---
+++ phpCRUD/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Read Products</title>
+
+    <!-- include material design CSS -->
+    <link rel="stylesheet" href="libs/css/materialize/css/materialize.min.css" />
+
+    <!-- include material design icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+    <!-- custom CSS -->
+    <style>
+        .width-30-pct{
+            width:30%;
+        }
+
+        .text-align-center{
+            text-align:center;
+        }
+
+        .margin-bottom-1em{
+            margin-bottom:1em;
+        }
+    </style>
+</head>
+<body>
+<div class="container" ng-app="myApp" ng-controller="usersCtrl">
+    <div class="row">
+        <div class="col s12">
+            <h4>Products</h4>
+
+        </div> <!-- end col s12 -->
+    </div> <!-- end row -->
+</div> <!-- end container -->
+<!-- page content and controls will be here -->
+
+<!-- include jquery -->
+<script type="text/javascript" src="libs/js/jquery.min.js"></script>
+
+<!-- material design js -->
+<script src="libs/css/materialize/js/materialize.min.js"></script>
+
+<!-- include angular js -->
+<script src="libs/js/angular.min.js"></script>
+
+<script>
+    // angular js codes will be here
+
+    // jquery codes will be here
+</script>
+
+</body>
+</html>
---
+++ phpCRUD/objects/users.php
@@ -0,0 +1,21 @@
+﻿<?php
+class Users{
+     
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+     
+    // object properties
+    public $FirstName;
+    public $LastName;
+    public $User_ID;
+    public $id_token;
+    public $Position;
+    public $status;
+    public $class;
+     
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+}
