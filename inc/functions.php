c3f8ab31b069225d70d50712a70043e69340f44e
YashminSainju <yashmin.sainju@selu.edu>
2016-11-21 12:36:25 -0600

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,53 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Wesley
+ * User: Yashmin
  * Date: 10/24/2016
  * Time: 8:12 PM
  */
+include 'database.php';
+try{
+    $active = $db->query("SELECT `FirstName`, `LastName` FROM `users` WHERE 'status' = 'Active'; ");
+    $inactive = $db->query("SELECT `FirstName`, `LastName` FROM `users` WHERE 'status' = 'Inactive';");
+    $all = $db->query("SELECT `FirstName`, `LastName` FROM `users`");
+}catch (Exception $e){
+    echo "Data could not be retrieved from the database";
+    exit;
+}
+
+$amembers = $active->fetchALL(PDO::FETCH_ASSOC);
+$imembers = $inactive->fetchALL(PDO::FETCH_ASSOC);
+$members = $all->fetchAll(PDO::FETCH_ASSOC);
+
+function addNewJob($jobTitle,$jobDescription){
+    try{
+        $db= new PDO(
+            "mysql:host=". DB_HOST .";dbname=". DB_NAME,
+            DB_USER,
+            DB_PASS
+        );
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        try{
+            $db->query("INSERT INTO `jobs`(JobTitle`, `JobDescription`, `JobContact`, `PostedBy`, `Time`) VALUES (".$jobTitle .",".$jobDescription.",NULL,NULL)");
+            echo "Job has been posted to the database";
+        }catch(Exception $e){
+            echo "Data could not be entered to the database";
+        }
+
+
+    } catch (Exception $e){
+        echo "Could not connect to the database.";
+        exit;
+    }
+
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
