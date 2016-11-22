63567263dba044135f7919d006b58c03939eaff4
Sleeper221 <moorma.aar@gmail.com>
2016-11-21 22:01:42 -0600

    Has Variables and Functions for members.
    Need to add function to read database. Will do when front-end is done!

--- inc/members/users.php
+++ inc/members/users.php
@@ -6,28 +6,98 @@ class Users{
     private $table_name = "users";
      
     // object properties
-    public $FirstName;
-    public $LastName;
-    public $Position;
-    public $status;
-    public $class;
+	protected $User_ID; 
+    protected $FirstName;
+    protected $LastName;
+    protected $Position;
+    protected $status;
+    protected $class;
      
     // constructor with $db as database connection
-    public function __construct($db){
+    protected function __construct($db){
         $this->conn = $db;
     }
 	
-	// delete the product
+	// create member
+	function create(){
+     
+		// query to insert record
+		$query = "INSERT INTO 
+					" . $this->table_name . "
+				SET 
+					FirstName=:FirstName, LastName=:LastName, Position=:Position, status=:status, class=:class";
+     
+		// prepare query
+		$stmt = $this->conn->prepare($query);
+ 
+		// posted values
+		$this->name=htmlspecialchars(strip_tags($this->FirstName));
+		$this->price=htmlspecialchars(strip_tags($this->LastName));
+
+ 
+		// bind values
+		$stmt->bindParam(":FirstName", $this->FirstName);
+		$stmt->bindParam(":LastName", $this->LastName);
+		$stmt->bindParam(":Position", $this->Position);
+		$stmt->bindParam(":status", $this->status);
+		$stmt->bindParam(":class", $this->class);
+     
+		// execute query
+		if($stmt->execute()){
+			return true;
+		}else{
+			echo "<pre>";
+				print_r($stmt->errorInfo());
+			echo "</pre>";
+ 
+			return false;
+		}
+	}
+	
+	// update the member
+	function update(){
+ 
+		// update query
+		$query = "UPDATE 
+					" . $this->table_name . "
+				SET 
+					FirstName=:FirstName, LastName=:LastName, Position=:Position, status=:status, class=:class
+				WHERE
+					User_ID = :User_ID";
+ 
+		// prepare query statement
+		$stmt = $this->conn->prepare($query);
+ 
+		// posted values
+		$this->name=htmlspecialchars(strip_tags($this->FirstName));
+		$this->price=htmlspecialchars(strip_tags($this->LastName));
+ 
+		// bind new values
+		$stmt->bindParam(":FirstName", $this->FirstName);
+		$stmt->bindParam(":LastName", $this->LastName);
+		$stmt->bindParam(":Position", $this->Position);
+		$stmt->bindParam(":status", $this->status);
+		$stmt->bindParam(":class", $this->class);
+     
+		// execute the query
+		if($stmt->execute()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	// delete the member
 	function delete(){
  
 		// delete query
-		$query = "DELETE FROM " . $this->table_name . " WHERE FirstName = ?";
+		$query = "DELETE FROM " . $this->table_name . " WHERE User_ID = ?";
      
 		// prepare query
 		$stmt = $this->conn->prepare($query);
      
 		// bind id of record to delete
-		$stmt->bindParam(1, $this->FirstName);
+		$stmt->bindParam(1, $this->User_ID);
  
 		// execute query
 		if($stmt->execute()){
