c20244adc33e6fc4d55f07b5ac4d99d076299351
YashminSainju <yashmin.sainju@selu.edu>
2016-11-27 17:29:04 -0600

    updated for login

--- inc/functions.php
+++ inc/functions.php
@@ -5,7 +5,7 @@
  * Date: 10/24/2016
  * Time: 8:12 PM
  */
-include 'database.php';
+/*include 'database.php';
 try{
     $active = $db->query("SELECT `FirstName`, `LastName` FROM `users` WHERE 'status' = 'Active'; ");
     $inactive = $db->query("SELECT `FirstName`, `LastName` FROM `users` WHERE 'status' = 'Inactive';");
@@ -49,5 +49,33 @@ function test_input($data) {
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
-}
+}*/
 
+include_once ("config.php");
+
+function sec_session_start() {
+    $session_name = 'sec_session_id';   // Set a custom session name
+    /*Sets the session name.
+     *This must come before session_set_cookie_params due to an undocumented bug/feature in PHP.
+     */
+    session_name($session_name);
+
+    $secure = true;
+    // This stops JavaScript being able to access the session id.
+    $httponly = true;
+    // Forces sessions to only use cookies.
+    if (ini_set('session.use_only_cookies', 1) === FALSE) {
+        header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+        exit();
+    }
+    // Gets current cookies params.
+    $cookieParams = session_get_cookie_params();
+    session_set_cookie_params($cookieParams["lifetime"],
+        $cookieParams["path"],
+        $cookieParams["domain"],
+        $secure,
+        $httponly);
+
+    session_start();            // Start the PHP session
+    session_regenerate_id(true);    // regenerated the session, delete the old one.
+}
