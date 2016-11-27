8127ee774fc7a158ff80d893964418da2eaa4220
YashminSainju <yashmin.sainju@selu.edu>
2016-11-27 17:53:09 -0600

    login processing

---
+++ inc/process_login.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yashmin
+ * Date: 11/27/2016
+ * Time: 5:47 PM
+ */
+include_once 'database.php';
+include_once 'functions.php';
+
+sec_session_start(); // Our custom secure way of starting a PHP session.
+
+if (isset($_POST['email'], $_POST['p'])) {
+    $email = $_POST['email'];
+    $password = $_POST['p']; // The hashed password.
+
+    if (login($email, $password, $db) == true) {
+        // Login success
+        header('Location: ../protected_page.php');
+    } else {
+        // Login failed
+        header('Location: ../index.php?error=1');
+    }
+} else {
+    // The correct POST variables were not sent to this page.
+    echo 'Invalid Request';
+}
