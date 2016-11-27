b1aae6a3ae0a1faa828ee9e41b72db0a4589f9d6
YashminSainju <yashmin.sainju@selu.edu>
2016-11-27 12:01:55 -0600

    add.php
    needs beautifying and scripts. header has to be routed using angular.

---
+++ inc/jobs/add.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yashmin
+ * Date: 11/27/2016
+ * Time: 10:16 AM
+ */
+
+include('../database.php');
+    if(isset($_POST['submit'])) {
+        $title = trim(filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING));
+        $description = trim(filter_input(INPUT_POST, "description", FILTER_SANITIZE_SPECIAL_CHARS));
+        $contact = trim(filter_input(INPUT_POST, "contact", FILTER_SANITIZE_EMAIL));
+        $query = "INSERT into jobs VALUES ('',:title,:description,:contact)";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(":title", $title);
+        $stmt->bindParam(":description", $description);
+        $stmt->bindParam(":contact", $contact);
+        if ($stmt->execute()) {
+            header('location: ../../admin.php');
+        }
+    }
+?>
+<html>
+    <body>
+    <fieldset style="width:300px;">
+        <form method="post" action="">
+            Job title: <input type="text" name="title"><br>
+            Job Description: <input type="text" name="description"><br>
+            Contact info: <input type = "text" name="contact"><br>
+            <br>
+            <input type="submit" name="submit">
+        </form>/
+    </fieldset>
+    </body>
+</html>
