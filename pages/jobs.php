77c71bfbfb5a8cd01c0c1762782a3f6454762e34
YashminSainju <yashmin.sainju@selu.edu>
2016-11-27 23:19:53 -0600

    Update jobs.php

--- pages/jobs.php
+++ pages/jobs.php
@@ -7,33 +7,37 @@
  */
 include('../inc/database.php');
 include('../inc/functions.php');
-/*sec_session_start();
-if(login_check($db) == true) {*/
+sec_session_start();
+if(login_check($db) == true) {
+try {
 	$query1 = $db->query("SELECT id,title,description,contact FROM jobs");
-?>
+	?>
 
-	
+
 	<h1><span class="text-muted">Job Offers: Get Connected!</span></h1>
-	
+
 	<table width="100%" class="table table-striped">
-		<tr>
-			<th>Title</th>
-			<th>Description</th>
-			<th>Contact Info</th>
-		</tr>
-		<?php
-			while($query2 = $query1->fetch(PDO::FETCH_ASSOC)) {
-				echo "
+	<tr>
+		<th>Title</th>
+		<th>Description</th>
+		<th>Contact Info</th>
+	</tr>
+	<?php
+	while ($query2 = $query1->fetch(PDO::FETCH_ASSOC)) {
+		echo "
 					<tr>
 						<td>" . $query2['title'] . "</td>
                         <td>" . $query2['description'] . "</td>
                         <td>" . $query2['contact'] . "</td>
 					</tr>";
-			}
+	}
+	}catch(Exception $e){
+	echo "No jobs listed at the moment";
+	}
 		?>
 	</table>
 	<?php
-/*} else {
+} else {
 	echo 'You are not authorized to access this page, please login.';
 }
-	?>*/
+	?>
