015d97d87060ad12c8b048827c7ee79a9efe4abc
YashminSainju <yashmin.sainju@selu.edu>
2016-11-27 21:29:29 -0600

    Create error.php

---
+++ error.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yashmin
+ * Date: 11/27/2016
+ * Time: 8:45 PM
+ */
+$error = filter_input(INPUT_GET, 'err', $filter = FILTER_SANITIZE_STRING);
+
+if (! $error) {
+    $error = 'Oops! An unknown error happened.';
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Secure Login: Error</title>
+    <link rel="stylesheet" href="styles/main.css" />
+</head>
+<body>
+<h1>There was a problem</h1>
+<p class="error"><?php echo $error; ?></p>
+</body>
+</html>
