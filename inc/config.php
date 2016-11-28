c9dc45b99bb41298b0c1e7c3e255492563b20aba
YashminSainju <yashmin.sainju@selu.edu>
2016-11-27 21:30:25 -0600

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -11,5 +11,9 @@ define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"]."/");
 
 define("DB_HOST","localhost");
 define("DB_NAME","acmsite");
-define("DB_USER","root");
-define("DB_PASS","");
+define("DB_USER","admin");
+define("DB_PASS","@wesdxc");
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE);
