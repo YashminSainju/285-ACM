c9f6028d8188cded1710475a6948c156197fcb36
YashminSainju <yashmin.sainju@selu.edu>
2016-11-27 19:16:13 -0600

    logout script

---
+++ inc/logout.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yashmin
+ * Date: 11/27/2016
+ * Time: 7:13 PM
+ */
+include_once 'functions.php';
+sec_session_start();
+
+// Unset all session values
+$_SESSION = array();
+
+// get session parameters
+$params = session_get_cookie_params();
+
+// Delete the actual cookie.
+setcookie(session_name(),
+    '', time() - 42000,
+    $params["path"],
+    $params["domain"],
+    $params["secure"],
+    $params["httponly"]);
+
+// Destroy session
+session_destroy();
+header('Location: ../index.php');
