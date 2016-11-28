6c2a34f6668e3d7f71dd44b3797b477d817a5410
YashminSainju <yashmin.sainju@selu.edu>
2016-11-28 17:17:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-    include_once 'inc/database.php';
-    include_once 'inc/functions.php';
+include_once 'inc/database.php';
+include_once 'inc/functions.php';
 
-    sec_session_start();
+sec_session_start();
 
-    if (login_check($db) == true) {
-        $logged = 'in';
-    } else {
-        $logged = 'out';
-    }
+if (login_check($db) == true) {
+    $logged = 'in';
+} else {
+    $logged = 'out';
+}
 
 ?>
 
@@ -19,9 +19,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ACM</title>
-	 <!-- Bootstrap core CSS 
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">-->
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
+    <!-- Bootstrap core CSS
+   <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
     <style>
         body{
             font-family: Montserrat, sans-serif;
@@ -77,18 +77,18 @@
             }
         }
     </style>
-     
-	
-	 <!-- JS (load angular, ui-router, and our custom js file) -->
+
+
+    <!-- JS (load angular, ui-router, and our custom js file) -->
     <script src="http://code.angularjs.org/1.2.13/angular.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js"></script>
     <script src="script.js"></script>
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug 
     <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">-->
-	
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    
+
     <link rel="stylesheet" href="resources/css/Navigation-with-Button1.css">
     <link rel="stylesheet" href="resources/css/Social-Icons.css">
     <link rel="stylesheet" href="resources/css/style.css">
@@ -96,38 +96,38 @@
     <link rel="stylesheet" href="resources/css/Team-Grid.css">
     <script type="text/JavaScript" src="inc/js/sha512.js"></script>
     <script type="text/JavaScript" src="inc/js/forms.js"></script>
-	
+
 </head>
 
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 <?php
-        if (isset($_GET['error'])) {
-            echo '<p class="error">Error Logging In!</p>';
-        }
+if (isset($_GET['error'])) {
+    echo '<p class="error">Error Logging In!</p>';
+}
 ?>
 
-    <div>
-        <nav class="navbar navbar-inverse navigation-clean-button">
-            <div class="container">
-                <div class="navbar-header"><a class="navbar-brand navbar-link" href="#myPage">ACM </a>
-                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                </div>
-                <div class="collapse navbar-collapse" id="navcol-1">
+<div>
+    <nav class="navbar navbar-inverse navigation-clean-button">
+        <div class="container">
+            <div class="navbar-header"><a class="navbar-brand navbar-link" href="#myPage">ACM </a>
+                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            </div>
+            <div class="collapse navbar-collapse" id="navcol-1">
 
-                    <ul class="nav navbar-nav">
-                        <li ><a ui-sref="home">Home </a></li>
-                        <li ><a href="https://www.acm.org">About </a></li>
-						<li ><a ui-sref="calendar">Calendar </a></li>
-                        <li ><a ui-sref="slugo">SLUGO </a></li>
-						<li ><a ui-sref="jobs">Job Offers </a></li>
-						<li ><a ui-sref="admin">Administrators </a></li>
-                        <li>
-                            <?php
-                            if (login_check($db) == true) {
-                                echo '<li><a>Welcome ' . htmlentities($_SESSION['username']) . '</a></li>
+                <ul class="nav navbar-nav">
+                    <li ><a ui-sref="home">Home </a></li>
+                    <li ><a href="https://www.acm.org">About </a></li>
+                    <li ><a ui-sref="calendar">Calendar </a></li>
+                    <li ><a ui-sref="slugo">SLUGO </a></li>
+                    <li ><a ui-sref="jobs">Job Offers </a></li>
+                    <li ><a ui-sref="admin">Administrators </a></li>
+                    <li>
+                        <?php
+                        if (login_check($db) == true) {
+                            echo '<li><a>Welcome ' . htmlentities($_SESSION['username']) . '</a></li>
                                 <li><a href="inc/logout.php">Log out</a></p>';
-                            }else{
-                                echo "
+                        }else{
+                            echo "
                                 <form action='inc/process_login.php' method='post' name='login_form' class = 'form-inline'>
                                 <div class = 'form-group'>
                                     <label for ='email'>Email </label>
@@ -136,54 +136,54 @@
                                 <div class = 'form-group'><label for='password'>Password </label>
                                     <input type='password' name='password' id='password' style='color: #000; font-size: 15px;' placeholder='Password' class='form-control'/>
                                 </div>
-                                <input type='button' class='btn btn-default' value='Login' onclick='formhash(this.form, this.form.password);' />
-                                <button class = 'btn btn-default'> <a href='register.php'>register</a></button>
+                                <input type='button' class='btn btn-default' style ='color: #333;background-color: #000; border-color: #ccc;' value='Login' onclick='formhash(this.form, this.form.password);' />
+                                <a href='register.php'class = 'btn btn-default' style ='color: #333;background-color: #000;border-color: #ccc;'>register</a>
 
 
                             </form>";
-                            }
-                            ?>
-                        </li>
-                    </ul>
-                    <!--<p class="navbar-text navbar-right actions"><a class="navbar-link login" ui-sref="#"><span class="glyphicon glyphicon-log-in"></span> Log In</a>-->
+                        }
+                        ?>
+                    </li>
+                </ul>
+                <!--<p class="navbar-text navbar-right actions"><a class="navbar-link login" ui-sref="#"><span class="glyphicon glyphicon-log-in"></span> Log In</a>-->
 
-                    </p>
-                </div>
+                </p>
             </div>
-        </nav>
-    </div>
-    
-    <div class="container-fluid"> <!-- angular insertion -->
-		
-		<!-- THIS IS WHERE WE WILL INJECT OUR CONTENT ============================== -->
-			<div ui-view></div>
-		
-	</div>
-	
-    <footer class="container-fluid text-center">
-        <a href="#myPage" title="To Top">
-            <span class="glyphicon glyphicon-chevron-up"></span>
-        </a>
-        <p class="text-center" class="text-muted">ACM · Southeastern Louisiana University</p>
-    </footer>
-    <div class="modal fade" role="dialog" tabindex="-1">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    <h4 class="modal-title">Modal Title</h4></div>
-                <div class="modal-body">
-                    <p>The content of your modal.</p>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-default" type="button" data-dismiss="modal">Close</button>
-                    <button class="btn btn-primary" type="button">Save</button>
-                </div>
+        </div>
+    </nav>
+</div>
+
+<div class="container-fluid"> <!-- angular insertion -->
+
+    <!-- THIS IS WHERE WE WILL INJECT OUR CONTENT ============================== -->
+    <div ui-view></div>
+
+</div>
+
+<footer class="container-fluid text-center">
+    <a href="#myPage" title="To Top">
+        <span class="glyphicon glyphicon-chevron-up"></span>
+    </a>
+    <p class="text-center" class="text-muted">ACM · Southeastern Louisiana University</p>
+</footer>
+<div class="modal fade" role="dialog" tabindex="-1">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Modal Title</h4></div>
+            <div class="modal-body">
+                <p>The content of your modal.</p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-default" type="button" data-dismiss="modal">Close</button>
+                <button class="btn btn-primary" type="button">Save</button>
             </div>
         </div>
     </div>
-    <script src="resources/js/jquery.min.js"></script>
-    <script src="resources/js/bootstrap.min.js"></script>
+</div>
+<script src="resources/js/jquery.min.js"></script>
+<script src="resources/js/bootstrap.min.js"></script>
 <script>
     $(document).ready(function(){
         // Add smooth scrolling to all links in navbar + footer link
