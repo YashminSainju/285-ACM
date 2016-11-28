5a0e1bca286f37e9c8982e8df5fd9c7138fd1656
YashminSainju <yashmin.sainju@selu.edu>
2016-11-28 14:12:55 -0600

    Create members.sql

---
+++ members.sql
@@ -0,0 +1,7 @@
+ 	CREATE TABLE `members` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(30) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `password` char(128) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1
