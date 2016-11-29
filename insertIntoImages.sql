b252836064e87fe37de1ebc4e2584e0866592e0a
YashminSainju <yashmin.sainju@selu.edu>
2016-11-28 22:50:35 -0600

    Create insertIntoImages.sql

---
+++ insertIntoImages.sql
@@ -0,0 +1,1 @@
+INSERT INTO `images`(`id`, `img`, `caption`) VALUES (NULL,LOAD_FILE( 'C:/xampp/htdocs/285-ACM-bf769227e1d81f9e14e3ba0cb0945f2a1790e112/resources/images/2015/@@_MG_2247.jpeg' ),'caption');
