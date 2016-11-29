371187de0bc8114ad3e339e29818f39b0a6a4f74
YashminSainju <yashmin.sainju@selu.edu>
2016-11-28 21:31:27 -0600

    Update userdelete.php

--- inc/members/userdelete.php
+++ inc/members/userdelete.php
@@ -1,12 +1,12 @@
 <?php
 include('../database.php');
-if(isset($_GET['ID']))
+if(isset($_GET['id']))
 {
-    $ID=$_GET['ID'];
-    $query1=$db->query("delete from users where ID='$ID'");
+    $id=$_GET['id'];
+    $query1=$db->query("delete from users where ID='$id'");
     if($query1)
     {
-        header('location: ../../admin.php');
+        header('location: ../../#/admin');
     }
 }
 ?>
